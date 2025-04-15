5fe83cadc02c4a6e8e96045bd058ff58619375c4
ismail-dev-code <ismail.online0385678@gmail.com>
2025-04-15 20:57:54 +0600

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import SimpleForm from './components/SimpleForm/SimpleForm'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>explore react form</h1>
+      <SimpleForm></SimpleForm>
+      
+    </>
+  )
+}
+
+export default App
