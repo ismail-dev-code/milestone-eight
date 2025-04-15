2336d19ee3cdf8bce8c6e3eacc1270f3a585f49b
ismail-dev-code <ismail.online0385678@gmail.com>
2025-04-15 21:38:12 +0600

    formAction, SimpleForm system added

---
+++ src/components/FormAction/FormAction.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const FormAction = () => {
+
+    const handleFormAction = (FormData) =>{
+        console.log(FormData.get('name'));
+        console.log(FormData.get('email'));
+    }
+
+    return (
+        <div>
+            <form action={handleFormAction}>
+
+                <input type="text" name='name' placeholder='your name' />
+                <br />
+                <input type="email" name="email" placeholder='Email'/>
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default FormAction;
--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(e.target.name.value);
+  console.log(e.target.email.value);
+};
 
 const SimpleForm = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="your name" />
+        <input type="email" name="email" placeholder="email" />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
 
 export default SimpleForm;
